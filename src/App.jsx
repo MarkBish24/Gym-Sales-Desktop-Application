d2081bdff9c4208b4ed6611f4fd20b625e29ad59
MarkBish24 <markbish24@gmail.com>
2025-06-13 16:54:26 -0700

    completed slide in page where user will input material

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,31 @@
 import "./App.css";
 import QueueItem from "./components/QueueItem.jsx";
+import AddListPage from "./components/AddListPage.jsx";
+import { useEffect, useState } from "react";
 
 const tempArray = [1, 2, 3, 4, 5];
 
 export default function App() {
+  const [showAddListPage, setShowAddListPage] = useState(false);
   return (
     <>
       <div className="header-container">
         <h1 className="header-title">The Gym-Sales-Bot</h1>
-        <button className="add-to-queue-btn">Add To Queue</button>
+        <button
+          className="add-to-queue-btn"
+          onClick={() => setShowAddListPage((prev) => !prev)}
+        >
+          Add To Queue
+        </button>
       </div>
       <div className="queue-container">
         {tempArray.map((element, index) => {
           return <QueueItem key={index} title={element} />;
         })}
       </div>
+      {showAddListPage ? (
+        <AddListPage setShowAddListPage={setShowAddListPage} />
+      ) : null}
     </>
   );
 }
--- src/components/AddListPage.jsx
+++ src/components/AddListPage.jsx
@@ -1,3 +1,25 @@
+import { easeIn, easeOut, motion } from "framer-motion";
+
 import "./AddListPage.css";
 
-export default function AddListPage() {}
+export default function AddListPage({ setShowAddListPage }) {
+  return (
+    <motion.div
+      initial={{ x: "-100%" }}
+      animate={{ x: 0 }}
+      exit={{ x: "-100%" }}
+      transition={{ duration: 0.5, ease: easeOut }}
+      className="add-list-page-container"
+    >
+      <div className="header-container">
+        <h1 className="header-title">Add List Page</h1>
+        <button
+          className="add-to-queue-btn"
+          onClick={() => setShowAddListPage((prev) => !prev)}
+        >
+          Leave
+        </button>
+      </div>
+    </motion.div>
+  );
+}
